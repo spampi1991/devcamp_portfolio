df4e34de2c26d6235b7ed4bf6c08e4b515c07fa6
Francesco <spampi1991@gmail.com>
2019-01-23 22:13:35 +0100

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,29 @@
 class PortfoliosController < ApplicationController
 
+  before_action :set_portfolio, only: [:show, :edit, :update]
+
   def index
     @portfolios_items = Portfolio.all
   end
 
   def new
     @portfolio_item = Portfolio.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def create
@@ -21,7 +39,11 @@ class PortfoliosController < ApplicationController
   end
 
   private
-  def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body)
-  end
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
+
+    def set_portfolio
+      @portfolio_item = Portfolio.find(params[:id])
+    end
 end
