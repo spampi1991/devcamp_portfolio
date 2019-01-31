362bc6bf0dac7dbefb7b2958587f58b677796863
Francesco <spampi1991@gmail.com>
2019-01-31 18:35:44 +0100

    Added defaults to Portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,12 @@ class Portfolio < ApplicationRecord
   def self.java
     where(subtitle: "Java")
   end
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/600x400.png"
+    self.thumb_image ||= "https://via.placeholder.com/350x200.png"
+  end
+
 end
